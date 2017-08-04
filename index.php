660aa9310570d7b3b7204bc461c69eebff903437
Berzeg07 <berezg86@gmail.com>
2017-08-04 10:53:43 +0300

    4.08

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>Главная</title>
+
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fonts.css">
+
+	
+</head>
+<body>
+
+	<div class="header-top">
+		<div class="header-top_container">
+			<span>+7(999)-999-99-99</span>
+		</div>
+	</div>
+
+	<div class="header-bottom">
+		<div class="header-top_container">
+			<div class="nav-box">
+				<div class="nav-box_item">
+					<ul class="nav">
+						<li><a href="#"><img src="img/lupa.png" alt=""></a></li>
+						<li><a href="#">о нас</a></li>
+						<li><a href="#">каталог</a></li>
+					</ul>
+				</div>
+
+				<div class="nav-box_item nav-box_item__center"><span>LEKS</span></div>
+
+				<div class="nav-box_item">
+					<ul class="nav">
+						<li><a href="#">корзина</a></li>
+						<li><a href="#">контакты</a></li>
+						<li><a href="#"><img src="img/kab.png" alt=""></a></li>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+
+
+
+
+
+
+
+		<!-- JS_BLOCK -->
+		<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+		<script src="libs/owl.carousel/owl.carousel.js"></script>
+		<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+		<script src="js/jquery.maskedinput.min.js"></script>
+		<script src="js/fonts.js"></script>
+		<script src="js/common.js"></script>
+
+
+	</body>
+	</html>
